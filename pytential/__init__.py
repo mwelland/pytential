dfb7da60aa2268652fe4ebf7108f6f95c4b90c9a
Mike Welland <mike@mikewelland.com>
2025-05-22 22:26:40 -0400

    Fix import statement for sympy_pytential in __init__.py

--- pytential/__init__.py
+++ pytential/__init__.py
@@ -1,4 +1,4 @@
 from .pytential import pytential
-from.sympy_pytential.sympy_pytential import sympy_pytential
+from .sympy_pytential.sympy_pytential import sympy_pytential
 
 from .read_write import read_pytential, write_pytential
